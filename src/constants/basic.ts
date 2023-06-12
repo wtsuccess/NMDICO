2a9d51e0397bdcf78e524bffcf2a12e6af808709
wtsuccess <white991120@gmail.com>
2023-06-12 07:49:29 -0500

    change contract address'

--- src/constants/basic.ts
+++ src/constants/basic.ts
@@ -1,3 +1,3 @@
-export const presaleContractAddress = "0x528344b968d1EbE9b5c2E6C6EDB2E441e38166C3";
+export const presaleContractAddress = "0xd0b3faaeF32154B32a96EfBB19Dd17226b278aE7";
 export const usdtContractAddress = "0x55d398326f99059ff775485246999027b3197955";
 
