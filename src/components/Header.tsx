6941368d04e999e2b95d957ea6835c81f017a8a4
wtsuccess <white991120@gmail.com>
2023-06-29 23:07:15 +0900

    convert web3modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,8 @@
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { ConnectionButton } from "./ConnectionButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import ConnectWallet from "./ConnectWallet";
 
 const Header = () => {
-  // const [isOpen, setIsOpen] = useState(false);
-
-  // const toggleDropdown = () => {
-  //   setIsOpen((isOpen) => !isOpen);
-  // };
-
   return (
     <header className="shadow-md py-3 px-2">
       <div className="mx-auto flex justify-between py-1 px-6 items-center text-white">
@@ -17,24 +10,8 @@ const Header = () => {
         <div className="flex items-center justify-between ">
           <div className="relative">
             <FontAwesomeIcon icon={faGlobe} className="pr-4" size="xl"/>
-            {/* {isOpen && (`
-              <div className="absolute top-[40px] left-[0]">
-                <span className="w-[25px] h-[25px] inline-block bg-black rotate-45 cursor-pointer z-0"></span>
-                <ul
-                  className="bg-black my-[-25px] mx-[-5px] z-20 py-2 w-[150px] px-2 relative text-[#b8add2]"
-                  onClick={(e) => {
-                    setIsOpen(false);
-                  }}
-                >
-                  <li className="cursor-pointer text-left  font-thin ">
-                    English
-                  </li>
-                  <li className="cursor-pointer text-left  font-thin">Hindi</li>
-                </ul>
-              </div>
-            )} */}
           </div>
-          <ConnectButton />
+          <ConnectWallet />
         </div>
       </div>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -1,79 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/styles/index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "@rainbow-me/rainbowkit/styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { WagmiConfig, configureChains, createClient } from 'wagmi';
-import { RainbowKitProvider, darkTheme, connectorsForWallets, cssStringFromTheme, lightTheme } from '@rainbow-me/rainbowkit';
-import {
-  injectedWallet,
-  walletConnectWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { bsc } from "wagmi/chains";
+import { WagmiConfig } from "wagmi";
 
-import { publicProvider } from 'wagmi/providers/public';
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Web3Modal } from "@web3modal/react";
+import { ethereumClient, projectId, wagmiClient } from "./constants/wagmi-config";
 
-import { trustWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
-
-const { chains, provider } = configureChains(
-  [bsc],
-  [publicProvider()]
-);
-
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [
-      injectedWallet({ chains }),
-      metaMaskWallet({ chains }),
-      trustWallet({ chains }),
-      walletConnectWallet({ chains }),
-    ],
-  },
-]);
-
-// const { connectors } = getDefaultWallets({
-//   appName: "NMD ICO",
-//   chains,
-// });
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
+const walletIds = {
+  metamask: "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+  trustWallet: "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>    
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
-            :root {
-              ${cssStringFromTheme(lightTheme)}
-            }
-
-            html[data-dark] {
-              ${cssStringFromTheme(darkTheme, {
-                extends: lightTheme,
-              })}
-            }
-          `,
+      <App />
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeMode="dark"
+        explorerRecommendedWalletIds={[
+          walletIds.metamask,
+          walletIds.trustWallet,
+        ]}
+        themeVariables={{
+          "--w3m-background-color": "#12B3A8",
+          "--w3m-accent-color": "#12B3A8",
         }}
       />
-        <ToastContainer />
-        <App />
-      </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>
 );
